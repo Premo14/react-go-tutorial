28c985178c0fa7f8676de53f021a837c0557b7ec
Anthony Premo <ajaipremo@gmail.com>
2024-08-01 12:39:10 -0400

    Fiber and Air Tools setup

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(200).JSON(fiber.Map{"msg": "Hello World"})
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
