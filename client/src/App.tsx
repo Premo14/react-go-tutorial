3c63ae32afaa987d7663f599362123272a35f53e
Anthony Premo <ajaipremo@gmail.com>
2024-08-05 14:28:01 -0400

    GET, PATCH, and POST requests completed in React

---
+++ client/src/App.tsx
@@ -0,0 +1,19 @@
+import {Container, Stack} from "@chakra-ui/react";
+import Navbar from "./components/Navbar.tsx";
+import TodoForm from "./components/TodoForm.tsx";
+import TodoList from "./components/TodoList.tsx";
+
+export const BASE_URL = "http://localhost:5000/api";
+function App() {
+    return (
+    <Stack h="100vh">
+      <Navbar />
+        <Container>
+            <TodoForm />
+            <TodoList />
+        </Container>
+    </Stack>
+  )
+}
+
+export default App
